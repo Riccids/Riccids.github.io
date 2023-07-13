4fb4cd7a29c7ffebc12122679fd760819c33994a
Riccids <108303179+Riccids@users.noreply.github.com>
2023-07-13 19:36:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.textColor = "#FFFFF";
 tg.MainButton.color = "#2cab37";
 
 let item = "";
 
-let btn1 = document.getElementById("btn1");
-let btn2 = document.getElementById("btn2");
-let btn3 = document.getElementById("btn3");
-let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
+let btn1 = document.getElementById("bttn1");
+let btn2 = document.getElementById("bttn2");
+let btn3 = document.getElementById("bttn3");
+let btn4 = document.getElementById("bttn4");
+let btn5 = document.getElementById("bttn5");
+let btn6 = document.getElementById("bttn6");
 
 btn1.addEventListener("click", function(){
     if(tg.MainButton.isVisible){
@@ -87,7 +87,7 @@ Telegram.WebApp.onEvent("mainButtonClicked", function(){
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p")
-p.innerText = `${tg.initDataUnsafe.user.first_name}`
-`${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.first_name}`
+`${tg.initDataUnsafe.last_name}`;
 
 usercard.appendChild(p);
